f4fedb4fec90f091f91368034126387205362ca4
Jamie Kyle <me@thejameskyle.com>
2018-10-09 13:54:55 -0700

    parallelism in jest

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,19 @@
 const config = require('jest-config');
 const isCI = require('is-ci');
 const ciParallelVars = require('ci-parallel-vars');
+const chunkd = require('chunkd');
 const child = require('child_process');
 
+let chunk = null;
+
+if (isCI && ciParallelVars && process.env.TEST_FILES) {
+  let tests = JSON.parse(process.env.TEST_FILES);
+  chunk = chunkd(tests, ciParallelVars.index, ciParallelVars.total);
+  console.log(`Tests are being split across ${ciParallelVars.total} nodes. Current node running ${chunk.length} of ${tests.length} tests`);
+}
+
 module.exports = {
-  testMatch: [
+  testMatch: chunk ? chunk : [
     ...config.defaults.testMatch,
     '**/__tests__/**/*.{ts,tsx}',
     '**/?(*.)+(spec|test).{ts,tsx}',
