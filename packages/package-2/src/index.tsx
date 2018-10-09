d06e8715018e4db624678adf64fd884fbaafcea8
Jamie Kyle <me@thejameskyle.com>
2018-10-08 17:34:57 -0700

    avoid cast syntax

--- packages/package-2/src/index.tsx
+++ packages/package-2/src/index.tsx
@@ -2,4 +2,5 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
-render(<App/>, (window as any).root);
+let win: any = window;
+render(<App/>, win.root);
